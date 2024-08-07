7d4f48193a3606bf4450b4804e0e2cfead8cc6ed
Andrey Nikolaev <a.nikolayev@msocialproduction.com>
2024-08-07 12:10:44 +0300

    #add #frontend - statistics block

--- src/app/i/Statistics.tsx
+++ src/app/i/Statistics.tsx
@@ -1,3 +1,5 @@
+'use client'
+
 import React from 'react'
 
 import Loader from '@/components/ui/Loader'
@@ -7,7 +9,25 @@ import { useProfile } from '@/hooks/useProfile'
 const Statistics = () => {
 	const { data, isLoading } = useProfile()
 
-	return isLoading ? <Loader /> : <div></div>
+	return isLoading ? (
+		<Loader />
+	) : (
+		<div className='grid grid-cols-4 gap-12 mt-7'>
+			{data?.statistics.length ? (
+				data.statistics.map(statistic => (
+					<div
+						className='bg-border/5 rounded p-layout text-center hover:-translate-y-3 transition-transform duration-500'
+						key={statistic.label}
+					>
+						<div className='text-xl'>{statistic.label}</div>
+						<div className='text-3xl font-semibold'>{statistic.value}</div>
+					</div>
+				))
+			) : (
+				<div>Statistics not loaded!</div>
+			)}
+		</div>
+	)
 }
 
 export default Statistics
